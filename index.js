19ba267d8f1ca5b94e9658d353d97f6ae3e991d0
Suryakant Mudke <suryakantmudke1234@gmail.com>
2023-09-27 03:10:05 +0530

    type conversion

--- index.js
+++ index.js
@@ -172,3 +172,45 @@ console.log(c);
 
 
 
+ /** Type Conversion */
+
+/*  *** Convert to String ***  */
+// String();
+
+console.log(String(100));
+console.log(String(true));
+console.log(String(null));
+console.log(String(undefined));
+
+// toString();
+
+console.log((123).toString());
+// console.log(null.toString());
+
+/** Convert to Number */
+// Number();
+
+console.log('***NUMBER***');
+
+console.log(Number('25'));
+console.log(Number('Value'));
+console.log(Number(false));
+
+// Unary + Operator --- works on Variables
+
+let l = '123falak';
+console.log(+l);
+
+// parseFloat();
+console.log(parseFloat('123.45f'));
+
+// parseInt();
+console.log(parseInt('123.45f'));
+
+/** Convert to Boolean */
+// Boolean();
+
+console.log(Boolean(undefined));
+
+console.log(Boolean(undefined));
+
